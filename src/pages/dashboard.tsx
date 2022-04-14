f51d728bf48dbcedbbac65d164bfbde43205c9a2
Guilherme Ramos <guiramos@DESKTOP-LO9LDT6.localdomain>
2022-04-14 16:49:16 -0300

    creating sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,16 @@
 import { Flex } from '@chakra-ui/react';
-import { Header } from 'components';
+import { Header, Sidebar } from 'components';
 
 const Dashboard = () => {
-  return <Header />;
+  return (
+    <Flex direction='column' h='100vh'>
+      <Header />
+      <Flex w='100%' my='6' maxW={1480} mx='auto' px='6'>
+        <Sidebar />
+        asdaskdj
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Dashboard;
