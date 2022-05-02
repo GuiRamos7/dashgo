332f1614d56d50538ffb98abec176db00f4544a3
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-05-02 09:58:17 -0300

    creating table of user for mobile

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,13 +18,13 @@ import { RiAddLine, RiPencilFill } from 'react-icons/ri';
 
 const UserList = () => {
   return (
-    <Box>
+    <Box >
       <Header />
 
       <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
 
-        <Box flex='1' bg='gray.800' borderRadius={8} p='8'>
+        <Box w="100%" flex='1' bg='gray.800' borderRadius={8} p='8'>
           <Flex mb='8' justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>
               Users
@@ -41,51 +41,54 @@ const UserList = () => {
               Create new
             </Button>
           </Flex>
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px='6' color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>User</Th>
-                <Th>Registration date</Th>
-                <Th width='8' />
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px='6'>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Guilherme Ramos</Text>
-                    <Text fontSize='sm' color='gray.300'>
-                      guioliveirarmos10@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>20 April, 2022</Td>
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='small'
-                    cursor='pointer'
-                    colorScheme='purple'
-                    alignItems='center'
-                    leftIcon={<Icon as={RiPencilFill} fontSize='16' />}
-                  >
-                    Edit
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+          <Box w="100%" overflowX="scroll" >
+            <Table colorScheme='whiteAlpha'>
+              <Thead>
+                <Tr>
+                  <Th px='6' color='gray.300' width='8'>
+                    <Checkbox colorScheme='pink' />
+                  </Th>
+                  <Th>User</Th>
+                  <Th>Registration date</Th>
+                  <Th width='8' />
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td px='6'>
+                    <Checkbox colorScheme='pink' />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight='bold'>Guilherme Ramos</Text>
+                      <Text fontSize='sm' color='gray.300'>
+                        guioliveirarmos10@gmail.com
+                      </Text>
+                    </Box>
+                  </Td>
+                  <Td>20 April, 2022</Td>
+                  <Td>
+                    <Button
+                      as='a'
+                      size='sm'
+                      fontSize='small'
+                      cursor='pointer'
+                      colorScheme='purple'
+                      alignItems='center'
+                      leftIcon={<Icon as={RiPencilFill} fontSize='16' />}
+                    >
+                      Edit
+                    </Button>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </Box>
+
           <Pagination />
         </Box>
-      </Flex>
-    </Box>
+      </Flex >
+    </Box >
   );
 };
 
