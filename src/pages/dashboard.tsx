4a992d3a5aeb09a36a189dbc85fa685f178c6448
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-05-02 09:26:27 -0300

    changing padding of dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -71,13 +71,13 @@ const Dashboard = () => {
           minChildWidth='320px'
           alignItems='flex-start'
         >
-          <Box p='8' pb='4' bg='gray.800' borderRadius={8}>
+          <Box p={['6', '8']} pb='4' bg='gray.800' borderRadius={8}>
             <Text fontSize='lg' mb='4'>
               Subscribes of the week
             </Text>
             <Chart series={series} options={options} type='area' height={160} />
           </Box>
-          <Box p='8' pb='4' bg='gray.800' borderRadius={8}>
+          <Box p={['6', '8']} pb='4' bg='gray.800' borderRadius={8}>
             <Text fontSize='lg' mb='4'>
               Open rate
             </Text>
