588ada609609715baa969f9b24241fc80ae07e9e
Guilherme Ramos <guioliveiraramos10@gmail.com>
2022-05-23 21:32:30 -0300

    adding axios to the project

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,14 +19,13 @@ import Link from 'next/link';
 import { useEffect } from 'react';
 import { RiAddLine, RiPencilFill } from 'react-icons/ri';
 import { useQuery } from 'react-query';
+import { api } from 'services/api';
 
 const UserList = () => {
   const { data, isLoading, error, isFetching } = useQuery(
     'users',
     async () => {
-      const response = await fetch('http://localhost:3000/api/users');
-
-      const data = await response.json();
+      const { data } = await api.get('users');
 
       const users = data.users.map((user) => ({
         id: user.id,
